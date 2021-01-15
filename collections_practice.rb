aed49f0cc5280544800309a3bd677a70230981a1
Wurdsmith <wurdsmithing@gmail.com>
2021-01-15 01:34:18 -0600

    commit tree 3b39694c9800d71e8175c8bf30bce1b880e02f76

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,44 @@
+def sort_array_asc(array)
+    new_array = array.sort
+end
 
+def sort_array_desc(array)
+    new_array = array.sort
+    reversed_array = new_array.reverse
+end
+
+def sort_array_char_count(array)
+    new_array = array.sort_by {|x| x.length}
+end
+
+def swap_elements(array)
+    array[1], array[2] = array[2], array[1]
+    array
+end
+
+def reverse_array(array)
+    new_array = array.reverse
+end
+
+def kesha_maker(array)
+    array.each{|x| x[2] = "$"}
+    array
+end 
+
+def find_a(array)
+    array.select{|x| x.start_with?("a")}
+end
+
+def sum_array(array)
+    array.sum
+end
+
+def add_s(array)
+    array.collect.with_index do |x,y|
+        if y != 1
+           x = x + "s"
+        else
+            x
+        end 
+    end    
+end
